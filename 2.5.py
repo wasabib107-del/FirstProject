9b657cef2a74af2c5df1db77f74f593934c9dc01
Pavel <pavel.seredkin0@gmail.com>
2026-01-06 20:30:49 +0300

    Auto backup: 2026-01-06 20:30:49

---
+++ 2.5.py
@@ -0,0 +1,21 @@
+import os
+import time
+from git import Repo
+
+WATCH_DIR = "/home/user/important_files"  # что бэкапим
+REPO_DIR = "./"        # git-репозиторий
+CHECK_INTERVAL = 60                        # секунд
+
+repo = Repo(REPO_DIR)
+
+def has_changes():
+    repo.git.add(A=True)
+    return repo.is_dirty()
+
+while True:
+    if has_changes():
+        commit_msg = f"Auto backup: {time.strftime('%Y-%m-%d %H:%M:%S')}"
+        repo.index.commit(commit_msg)
+        repo.remotes.origin.push()
+        print("Backup done")
+    time.sleep(CHECK_INTERVAL)
